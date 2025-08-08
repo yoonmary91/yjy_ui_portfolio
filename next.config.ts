264c9a769ad4346993036d53a56faa3cd9f85b82
YJY <gtgyoon301@gmail.com>
2025-08-08 23:01:55 +0900

    chore: ignore eslint during builds

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ 빌드 시 ESLint 에러 무시
+  },
 };
 
 export default nextConfig;
